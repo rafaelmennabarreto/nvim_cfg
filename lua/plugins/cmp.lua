415e40c0e38981c3b50ec3f5b21d9bb57d965345
Rafael Barreto <rafaelbar@abcbrasil.com.br>
2025-02-28 20:05:08 -0300

    config(cmp): add new autocomplete format

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,26 +2,24 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "hrsh7th/cmp-emoji",
+    "petertriho/cmp-git",
+    "hrsh7th/cmp-nvim-lua",
   },
 
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
+    local luasnip = require("luasnip")
+    local cmp = require("cmp")
+
     local has_words_before = function()
-      unpack = unpack or table.unpack
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
-    local luasnip = require("luasnip")
-    local cmp = require("cmp")
-
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
           cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- this way you will only jump inside the snippet region
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
         elseif has_words_before() then
@@ -30,6 +28,7 @@ return {
           fallback()
         end
       end, { "i", "s" }),
+
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
@@ -39,6 +38,8 @@ return {
           fallback()
         end
       end, { "i", "s" }),
+
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
     })
 
     local source_names = {
@@ -50,79 +51,96 @@ return {
       luasnip = "[Snippet]",
       buffer = "[Buffer]",
       tmux = "[TMUX]",
-      codeium = "[IA]",
+      codeium = "[AI]",
+      git = "[Git]",
+      nvim_lua = "[Lua]",
     }
 
     local icons = {
-      Constructor = "  ",
-      Function = "  ",
-      Keyword = "  ",
-      Method = "  ",
-      Module = "  ",
-      Snippet = " 󰹷 ",
-      Text = " 󰟵 ",
-      Variable = " 󰚇 ",
-      Field = "  ",
-      Property = "  ",
-      Constant = "  ",
-      Array = "  ",
-      Boolean = "  ",
-      Color = "  ",
-      Enum = "  ",
-      EnumMember = "  ",
-      Event = "  ",
-      File = "  ",
-      Folder = "  ",
-      Interface = "  ",
-      Key = "  ",
-      Namespace = "  ",
-      Null = " ﳠ ",
-      Number = "  ",
-      Object = "  ",
-      Operator = "  ",
-      Package = "  ",
-      Reference = "  ",
-      String = "  ",
-      Struct = "  ",
-      TypeParameter = "  ",
-      Unit = "  ",
-      Value = "  ",
-      Win = "  ",
-      Codeium = "  ",
-      Class = "  ",
+      Text = "󰉿", -- Letra A estilizada
+      Method = "󰆧", -- Função com argumentos
+      Function = "󰊕", -- Ícone de função
+      Constructor = "", -- Chave inglesa para construtores
+      Field = "󰜢", -- Campo/atributo
+      Variable = "󰀫", -- Variável
+      Class = "󰠱", -- Classe
+      Interface = "", -- Interface (estrutura)
+      Module = "", -- Módulo/Pacote
+      Property = "󰜢", -- Propriedade (similar a Field)
+      Unit = "", -- Unidade
+      Value = "󰎠", -- Valor constante
+      Enum = "", -- Enumeração (lista)
+      Keyword = "󰌋", -- Palavra-chave
+      Snippet = "", -- Trecho de código
+      Color = "󰏘", -- Paleta de cores
+      File = "󰈙", -- Arquivo
+      Reference = "󰈇", -- Referência
+      Folder = "󰉋", -- Pasta
+      EnumMember = "", -- Membro de Enum
+      Constant = "󰏿", -- Constante
+      Struct = "", -- Estrutura (parecido com classe)
+      Event = "", -- Evento (relâmpago)
+      Operator = "󰆕", -- Operador (matemático)
+      TypeParameter = "󰊄", -- Parâmetro de tipo
+      Namespace = "󰌗", -- Namespace
+      Package = "󰏓", -- Pacote
+      Boolean = "", -- Booleano (V/F)
+      Number = "󰎠", -- Número
+      String = "󰀬", -- String (texto)
+      Array = "󰅪", -- Array (lista)
+      Object = "󰀚", -- Objeto genérico
+      Null = "󰟢", -- Nulo (null)
+      Key = "󰌋", -- Chave (mapa/dicionário)
+      Codeium = "󰘦", -- Ícone especial para IA (Codeium)
+      Win = "󰍰", -- Janela
     }
 
     opts.window = {
       completion = {
         border = "rounded",
-        --col_offset = -3,
-        --side_padding = 0,
         winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
       },
       documentation = {
         border = "rounded",
-        --col_offset = -3,
-        --side_padding = 0,
         winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
       },
-      --documentation = cmp.config.window.bordered(),
     }
 
     opts.formatting = {
-      mode = "symbol_text",
-      fields = { "abbr", "kind", "menu" },
+      fields = { "kind", "abbr", "menu" },
       expandable_indicator = true,
       max_width = 0,
       format = function(entry, vim_item)
-        vim_item.dup = 1
-        vim_item.abbr = string.sub(vim_item.abbr, 1, 50 - 1)
+        vim_item.abbr = string.sub(vim_item.abbr, 1, 50)
         vim_item.menu = source_names[entry.source.name]
-        vim_item.kind = (icons[vim_item.kind] or vim_item.kind)
-
+        vim_item.kind = (icons[vim_item.kind] or vim_item.kind) .. " "
         return vim_item
       end,
     }
 
-    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+      { name = "emoji" },
+      { name = "git" },
+      { name = "nvim_lua" },
+    }))
+
+    opts.sorting = {
+      comparators = {
+        function(entry1, entry2)
+          if entry1.completion_item.label == entry2.completion_item.label then
+            return false -- Remove entradas duplicadas
+          end
+          return nil -- Continua ordenação normal
+        end,
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.score,
+        cmp.config.compare.recently_used,
+        cmp.config.compare.locality,
+        cmp.config.compare.kind,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
+    }
   end,
 }
